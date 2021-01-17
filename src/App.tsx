84d40fcc2003aa0e3d90aeb31ad51705c4a65d0a
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-01-17 11:22:00 +0100

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect, Link } from 'react-router-dom';
 
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-import MovieList from "./components/MovieList";
-import MovieEditor from "./components/MovieEditor";
-import reducers from "./reducers";
+import MovieList from './components/MovieList';
+import MovieEditor from './components/MovieEditor';
+import reducers from './reducers';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
@@ -17,9 +17,15 @@ const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div className="container">
-        <h1>Popular movies</h1>
-
         <BrowserRouter>
+          <nav
+            className="navbar navbar-dark bg-dark"
+            style={{ marginBottom: 24 }}
+          >
+            <Link to="/" className="navbar-brand">
+              Popular movies
+            </Link>
+          </nav>
           <Switch>
             <Route path="/movies">
               <MovieList />
