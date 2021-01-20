186cab4c222c79dcdb1c43baa6dd6c28634a1da6
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-01-20 19:23:07 +0100

    Fake network requests

---
+++ .presentation-buddy/04-wait-network.spec.js
@@ -0,0 +1,43 @@
+context('cy.intercept() and data returned', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/popular-movies', {
+      fixture: './popular-movies.json',
+    }).as('popular-movies');
+
+    cy.visit('/movies');
+  });
+
+  it('loads the complete data set', () => {
+    return cy.wait('@popular-movies').then((xhr) => {
+      const movies = xhr.response.body;
+      console.log(movies);
+
+      movies.forEach((movie, index) => {
+        cy.findAllByRole('rowgroup')
+          .eq(1)
+          .findAllByRole('row')
+          .eq(index)
+          .should('have.attr', 'id', `movie-${movie.id}`);
+
+        cy.findAllByRole('rowgroup')
+          .eq(1)
+          .findAllByRole('row')
+          .eq(index)
+          .within((row) => {
+            cy.wrap(row)
+              .findAllByRole('cell')
+              .eq(0)
+              .should('have.text', movie.title);
+            cy.wrap(row)
+              .findAllByRole('cell')
+              .eq(1)
+              .should('have.text', movie.vote_average);
+            cy.wrap(row)
+              .findAllByRole('cell')
+              .eq(2)
+              .should('have.text', 'Edit');
+          });
+      });
+    });
+  });
+});
